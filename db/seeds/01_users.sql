c5e9e3d2fba1f14dd27ea56292e4c2898eea0470
Matt <mgstankov30@gmail.com>
2022-04-29 14:26:28 -0400

    add 8 user date for testing and demoing

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,9 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (email, username, first_name, last_name) VALUES ('Alice@example.com', 'alice1', 'Alice', 'Andrews');
+INSERT INTO users (email, username, first_name, last_name) VALUES ('Kira@example.com', 'kira21', 'Kira', 'K');
+INSERT INTO users (email, username, first_name, last_name) VALUES ('Alex@example.com', 'alex19', 'Alex', 'Nameson');
+INSERT INTO users (email, username, first_name, last_name) VALUES ('Ashley@example.com', 'ashley2019', 'Ashley', 'White');
+INSERT INTO users (email, username, first_name, last_name) VALUES ('Arianne@example.com', 'ata30', 'Arianne', 'Rockets');
+INSERT INTO users (email, username, first_name, last_name) VALUES ('Gnocchi@example.com', 'gbas1', 'Gnocchi', 'Bocconcini');
+INSERT INTO users (email, username, first_name, last_name) VALUES ('Ravioli@example.com', 'raas1', 'Ravioli', 'Alfredo');
+INSERT INTO users (email, username, first_name, last_name) VALUES ('Brad@example.com', 'brobro22', 'Brad', 'Broad');
