fc76844750cb897f7cc3332be4e1c10971efff5f
Matt <mgstankov30@gmail.com>
2022-04-29 14:28:41 -0400

    add date for record images to connect and correspond with records data

---
+++ db/seeds/04_record_images.sql
@@ -0,0 +1,8 @@
+INSERT INTO record_images (record_id, img_url) VALUES ('1', 'https://upload.wikimedia.org/wikipedia/en/9/97/Bombay_Bicycle_Club_-_Everything_Else_Has_Gone_Wrong.png');
+INSERT INTO record_images (record_id, img_url) VALUES ('2', 'https://images.roughtrade.com/product/images/files/000/238/629/hero/cover.ki_%2812%29.jpg?1646948837');
+INSERT INTO record_images (record_id, img_url) VALUES ('3', 'https://static.stereogum.com/uploads/2022/03/unnamed-2-1648767754.jpg');
+INSERT INTO record_images (record_id, img_url) VALUES ('4', 'https://cdn.albumoftheyear.org/album/184733-jet-blue.jpg');
+INSERT INTO record_images (record_id, img_url) VALUES ('5', 'https://m.media-amazon.com/images/I/81hXZ0A0OZS._SS500_.jpg');
+INSERT INTO record_images (record_id, img_url) VALUES ('6', 'https://f4.bcbits.com/img/a1025522302_5.jpg');
+INSERT INTO record_images (record_id, img_url) VALUES ('7', 'https://i.scdn.co/image/ab67616d0000b2736b9afebbb0db6938d5170a4c');
+INSERT INTO record_images (record_id, img_url) VALUES ('8', 'https://m.media-amazon.com/images/I/91OeYnLoCJL._AC_SL1500_.jpg');
