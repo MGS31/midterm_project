55b8d5efcdf3ace39233239921959a67d735688a
Rachel PR <rplourder@gmail.com>
2022-04-30 14:54:12 -0300

    Fixed routes for my_collection and my_turntable

--- server.js
+++ server.js
@@ -37,25 +37,44 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const myCollectionRoutes = require("./routes/my_collection");
+const myCollectionAPIRoutes = require("./routes/api/my_collection");
+const myTurntableRoutes = require("./routes/my_turntable");
+const myTurntableAPIRoutes = require("./routes/api/my_turntable");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+app.use("/api/mycollection", myCollectionAPIRoutes(db));
+app.use("/api/myturntable", myTurntableAPIRoutes(db));
+
+//Non API routes
+app.use("/mycollection", myCollectionRoutes(db));
+app.use("/myturntable", myTurntableRoutes(db));
+
 // Note: mount other resources here, using the same pattern above
 
 // Home page
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
 
-app.get("/sell-a-record", (req, res) => {
-  res.render("sell_a_record");
-});
+// app.get("/sell-a-record", (req, res) => {
+//   res.render("sell_a_record");
+// });
+
+// app.get("/mycollection", (req, res) => {
+//   res.render("my_collection");
+// });
+
+// app.get("/myturntable", (req, res) => {
+//   res.render("my_turntable");
+// });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+  console.log(`Sell-a-record app listening on port ${PORT}`);
 });
