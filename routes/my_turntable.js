76f21d23f8e95bebbdb5a5f7398c0e63e5800f88
Rachel PR <rplourder@gmail.com>
2022-05-01 11:43:05 -0300

    Add functionality to get record information and comments

--- routes/my_turntable.js
+++ routes/my_turntable.js
@@ -3,8 +3,31 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    res.render("my_turntable");
+    const myListing = `
+    SELECT title, artist_name, price, description,listing_views, record_images.img_url
+    FROM records
+    JOIN record_images ON records.id = record_id
+    JOIN users ON seller_id = users.id
+    WHERE username = 'raas1' LIMIT 1`;
+    db.query(myListing, (error, results) => {
+      if (error) {
+        throw error;
+      }
+      const recordComments = `
+      SELECT comments.description, users.username
+      FROM comments
+      JOIN users on user_id = users.id
+      WHERE username = 'raas1'`;
+      db.query(recordComments, (error, results2) => {
+        if (error) {
+          throw error;
+        }
+        res.render("my_turntable", {
+          records: results.rows,
+          comments: results2.rows,
+        });
+      });
+    });
   });
-  console.log(router);
   return router;
 };
