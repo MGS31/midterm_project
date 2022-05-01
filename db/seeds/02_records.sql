3caaa8d89c360d7678bc6e502fd297c5d35033b1
Rachel PR <rplourder@gmail.com>
2022-05-01 17:43:52 -0300

    Add in seed info for records, fav_records, comments

--- db/seeds/02_records.sql
+++ db/seeds/02_records.sql
@@ -1,16 +1,16 @@
-INSERT INTO records (title, price, genre, artist_name, description, listing_views, seller_id)
-VALUES ('Everything Else Has Gone Wrong', '10', 'Alternative', 'Bombaye Bicycle Club', 'Really great Album! a sold return from an amazing band!', 0, 7);
-INSERT INTO records (title, price, genre, artist_name, description, listing_views, seller_id)
-VALUES ('Close Distance', '12', 'Jazz', 'FloFilz', 'Really great Album! a sold entry for this artist and a must have!', 0, 7);
-INSERT INTO records (title, price, genre, artist_name, description, listing_views, seller_id)
-VALUES ('As It Was', '5', 'Pop', 'Harry Styles', 'The latest single and a real treat!', 0, 6);
-INSERT INTO records (title, price, genre, artist_name, description, listing_views, seller_id)
-VALUES ('Jet Blue', '15', 'Alternative', 'THREE1989', 'Favourite thing to come out of Japan!', 0, 5);
-INSERT INTO records (title, price, genre, artist_name, description, listing_views, seller_id)
-VALUES ('Where U Are', '9', 'Alternative', 'Throttle', 'It is both Chill and gets you bumping!!', 0, 1);
-INSERT INTO records (title, price, genre, artist_name, description, listing_views, seller_id)
-VALUES ('Celeste', '12', 'Jazz', 'Psalm Trees', 'Favourite thing to relax to, honestly a must have!', 0, 2);
-INSERT INTO records (title, price, genre, artist_name, description, listing_views, seller_id)
-VALUES ('Bump This', '3', 'Alternative', 'Danny Gonzalez', 'Great music and always makes me laugh', 0, 8);
-INSERT INTO records (title, price, genre, artist_name, description, listing_views, seller_id)
-VALUES ('Flower Boy', '19', 'Hip-Hop', 'Tyler, The Creator', 'Straight up banger! Can never go wrong with Tyle, the creator', 0, 2);
+INSERT INTO records (title, price, genre, artist_name, description, sold, listing_views, seller_id)
+VALUES ('Everything Else Has Gone Wrong', '10', 'Alternative', 'Bombaye Bicycle Club', 'Really great Album! a sold return from an amazing band!',false, 0, 7);
+INSERT INTO records (title, price, genre, artist_name, description, sold, listing_views, seller_id)
+VALUES ('Close Distance', '12', 'Jazz', 'FloFilz', 'Really great Album! a sold entry for this artist and a must have!',false, 0, 7);
+INSERT INTO records (title, price, genre, artist_name, description, sold, listing_views, seller_id)
+VALUES ('As It Was', '5', 'Pop', 'Harry Styles', 'The latest single and a real treat!',false, 0, 6);
+INSERT INTO records (title, price, genre, artist_name, description, sold, listing_views, seller_id)
+VALUES ('Jet Blue', '15', 'Alternative', 'THREE1989', 'Favourite thing to come out of Japan!',false, 0, 5);
+INSERT INTO records (title, price, genre, artist_name, description, sold, listing_views, seller_id)
+VALUES ('Where U Are', '9', 'Alternative', 'Throttle', 'It is both Chill and gets you bumping!!',false, 0, 1);
+INSERT INTO records (title, price, genre, artist_name, description, sold, listing_views, seller_id)
+VALUES ('Celeste', '12', 'Jazz', 'Psalm Trees', 'Favourite thing to relax to, honestly a must have!',false, 0, 2);
+INSERT INTO records (title, price, genre, artist_name, description, sold, listing_views, seller_id)
+VALUES ('Bump This', '3', 'Alternative', 'Danny Gonzalez', 'Great music and always makes me laugh',false, 0, 8);
+INSERT INTO records (title, price, genre, artist_name, description, sold, listing_views, seller_id)
+VALUES ('Flower Boy', '19', 'Hip-Hop', 'Tyler, The Creator', 'Straight up banger! Can never go wrong with Tyle, the creator',false, 0, 2);
---
+++ db/seeds/05_comments.sql
@@ -0,0 +1,1 @@
+INSERT INTO comments (record_id,user_id, description) VALUES ('1','7','Meow, what a nice sounding record!')
