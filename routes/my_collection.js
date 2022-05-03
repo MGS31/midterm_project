f31268fde67b73496433d451c8d2ad2ea5351ee0
Rachel PR <rplourder@gmail.com>
2022-05-03 14:15:10 -0300

    Fix functionality for delete and sold requests

--- routes/my_collection.js
+++ routes/my_collection.js
@@ -4,28 +4,28 @@ const router = express.Router();
 module.exports = (db) => {
   router.get("/", (req, res) => {
     const myListing = `
-    SELECT title, artist_name, price, sold,listing_views, record_images.img_url
+    SELECT records.id,title, artist_name, price, sold,listing_views, record_images.img_url
     FROM records
     JOIN record_images ON records.id = record_id
     JOIN users ON seller_id = users.id
-    WHERE username = 'raas1' LIMIT 3`;
+    WHERE username = 'kira21' LIMIT 3`;
     db.query(myListing, (error, results) => {
       if (error) {
         throw error;
       }
       const myFavourites = `
-      SELECT title, artist_name, price,sold,listing_views,record_images.img_url
+      SELECT records.id,title, artist_name, price,sold,listing_views,record_images.img_url
       FROM records
       JOIN record_images ON record_id = records.id
       JOIN favourite_records ON favourite_records.record_id = records.id
       JOIN users ON user_id = users.id
-      WHERE users.id = 7`;
+      WHERE users.id = 2`;
       db.query(myFavourites, (error, results2) => {
         if (error) {
           throw error;
         }
         const countListings = `
-        SELECT count(*) from records where seller_id = 7;`;
+        SELECT count(*) from records where seller_id = 2;`;
         db.query(countListings, (error, results3) => {
           if (error) {
             throw error;
@@ -34,14 +34,14 @@ module.exports = (db) => {
           //   console.log(count);
           // }
           const countFavourites = `
-            SELECT count(*) from favourite_records where user_id = 7;`;
+            SELECT count(*) from favourite_records where user_id = 2;`;
           db.query(countFavourites, (error, results4) => {
             if (error) {
               throw error;
             }
-            // for (let count of results3.rows) {
-            //   console.log(count);
-            // }
+            for (let count of results4.rows) {
+              console.log(count);
+            }
             res.render("my_collection", {
               records: results.rows,
               favourites: results2.rows,
@@ -53,10 +53,16 @@ module.exports = (db) => {
       });
     });
   });
-  router.post("/", (req, res) => {
-    let deledtedRecordID = [1];
+  router.delete("/:id", (req, res) => {
+    let deledtedRecordID = [req.params.id];
     let deleteRecord = `DELETE FROM records WHERE id = $1`;
     db.query(deleteRecord, deledtedRecordID);
+    res.redirect("/mycollection");
+  });
+  router.post("/:id", (req, res) => {
+    let soldRecordID = [req.params.id];
+    let soldRecord = `UPDATE records SET sold = true WHERE id = $1;`;
+    db.query(soldRecord, soldRecordID);
     res.redirect("/mycollection");
   });
   return router;
