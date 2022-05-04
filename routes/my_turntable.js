7fdd5fcf2490016e3ba54ec6c434b7f15e035a79
Rachel PR <rplourder@gmail.com>
2022-05-04 19:07:21 -0300

    Fix styling and functionality for comments section

--- routes/my_turntable.js
+++ routes/my_turntable.js
@@ -2,14 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
+  router.get("/:id", (req, res) => {
+    let recordID = [req.params.id];
     const myListing = `
-    SELECT title, artist_name, price, description,sold,listing_views, record_images.img_url
+    SELECT records.id,title, artist_name, price, description,sold,listing_views, record_images.img_url
     FROM records
     JOIN record_images ON records.id = record_id
     JOIN users ON seller_id = users.id
-    WHERE record_id = 3 LIMIT 1`;
-    db.query(myListing, (error, results) => {
+    WHERE record_id = $1 LIMIT 1`;
+    db.query(myListing, recordID, (error, results) => {
       if (error) {
         throw error;
       }
@@ -17,14 +18,14 @@ module.exports = (db) => {
       SELECT comments.description, users.username
       FROM comments
       JOIN users on user_id = users.id
-      WHERE record_id = 3`;
-      db.query(recordComments, (error, results2) => {
+      WHERE record_id = $1`;
+      db.query(recordComments, recordID, (error, results2) => {
         if (error) {
           throw error;
         }
         const countComments = `
-        SELECT count(*) FROM comments WHERE record_id = 3`;
-        db.query(countComments, (error, results3) => {
+        SELECT count(*) FROM comments WHERE record_id = $1`;
+        db.query(countComments, recordID, (error, results3) => {
           if (error) {
             throw error;
           }
@@ -36,14 +37,18 @@ module.exports = (db) => {
         });
       });
     });
+    // router.get("/:id", (req, res) => {
+    //   res.redirect("/mycollection");
+    // });
   });
-  router.post("/", (req, res) => {
+  router.post("/:id", (req, res) => {
+    // let recordID = req.params.id;
     let userComment = req.body["newComment"];
-    let commentValues = [3, 6, userComment];
+    let commentValues = [4, 6, userComment];
     let newComment = `
     INSERT INTO comments (record_id,user_id, description) VALUES ($1 ,$2 ,$3)`;
     db.query(newComment, commentValues);
-    res.redirect("/myturntable").catch((err) => {
+    res.redirect("/myturntable/4").catch((err) => {
       console.log(err.message);
     });
   });
