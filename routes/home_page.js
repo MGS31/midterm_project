6af1a8749420abdc789af1c25e9ac0d125f15656
Matt <mgstankov30@gmail.com>
2022-05-05 18:29:21 -0400

    update route to add a randomizer for the favourites display

--- routes/home_page.js
+++ routes/home_page.js
@@ -2,14 +2,14 @@ const express = require("express");
 const res = require("express/lib/response");
 const router = express.Router();
 
-function getRandomNumberBetween(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+// function getRandomNumberBetween(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// }
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
     const sql =
-    `SELECT records.id, title, artist_name, price, sold, description, listing_views, record_images.img_url
+    `SELECT records.id, title, seller_id, artist_name, price, sold, description, listing_views, record_images.img_url
     FROM records
     JOIN record_images ON record_id = records.id
     ORDER BY RANDOM()
@@ -18,23 +18,24 @@ module.exports = (db) => {
       if(error) {
         throw error;
       }
-      let user = getRandomNumberBetween(1, 8);
+      // let user = getRandomNumberBetween(1, 8);
        const sql2 =
       `SELECT records.id, title, price, record_images.img_url
       FROM records
       JOIN record_images ON record_id = records.id
       JOIN favourite_records ON favourite_records.record_id = records.id
       JOIN users ON user_id = users.id
-      WHERE users.id = ${user}
+      WHERE users.id = 1
+      ORDER BY RANDOM()
       LIMIT 4;`
       db.query(sql2, (error,results2) => {
         if(error) {
           throw error;
         }
         res.render("index.ejs", { records: results.rows, favourites : results2.rows })
+        })
       })
     })
-  })
   //console.log(router);
   return router;
 };
